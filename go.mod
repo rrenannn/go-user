76a0314782bd8953220fca457ed0f930047a8125
Renan <renan.gamero1313@gmail.com>
2025-09-09 23:49:10 -0300

    repository added

---
+++ go.mod
@@ -0,0 +1,3 @@
+module github.com/rrenannn/go-user
+
+go 1.25.0
--- internal/user/repository.go
+++ internal/user/repository.go
@@ -1,8 +1,43 @@
 package user
 
-import "database/sql"
+import (
+	"context"
+	"database/sql"
+
+	db "github.com/rrenannn/go-user/db/sqlc"
+)
+
+type RepositoryInterface interface {
+	CreateUser(ctx context.Context, arg db.CreateUserParams) (db.User, error)
+	GetUserById(ctx context.Context, id int64) (db.User, error)
+	GetUserByEmail(ctx context.Context, email string) (db.User, error)
+	ResetPassword(ctx context.Context, arg db.ResetPasswordParams) error
+}
 
 type Repository struct {
 	Conn    *sql.DB
 	Queries *db.Queries
+}
+
+func NewRepository(conn *sql.DB, queries *db.Queries) *Repository {
+	return &Repository{
+		Conn:    conn,
+		Queries: queries,
+	}
+}
+
+func (b *Repository) CreateUser(ctx context.Context, arg db.CreateUserParams) (db.User, error) {
+	return b.Queries.CreateUser(ctx, arg)
+}
+
+func (b *Repository) GetUserById(ctx context.Context, id int64) (db.User, error) {
+	return b.Queries.GetUserById(ctx, id)
+}
+
+func (b *Repository) GetUserByEmail(ctx context.Context, email string) (db.User, error) {
+	return b.Queries.GetUserByEmail(ctx, email)
+}
+
+func (b *Repository) ResetPassword(ctx context.Context, arg db.ResetPasswordParams) error {
+	return b.Queries.ResetPassword(ctx, arg)
 }
